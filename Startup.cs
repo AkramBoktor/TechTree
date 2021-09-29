c9c1c8f00213865e780330025e991d5150779678
Akram-Boktor <35446384+AkramBoktor@users.noreply.github.com>
2021-09-29 19:28:16 +0200

    Add files via upload
    add - admin login form

--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,7 @@ namespace TechTreeMVCWebApplication
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
