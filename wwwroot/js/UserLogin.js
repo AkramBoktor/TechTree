dda3dd141e1d0b47ed0f4a4f7cbc2235ac6b1707
Akram-Boktor <35446384+AkramBoktor@users.noreply.github.com>
2021-10-06 21:40:42 +0200

    Add files via upload

---
+++ wwwroot/js/UserLogin.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+
+    var userLoginButton = $("#UserLoginModel button[name='login ']").click(onUserLoginClick);
+
+    function onUserLoginClick() {
+        var userUrl = 'UserAuth/Login';
+        var antiForgeryToken = $("#UserLoginModal input[name='__RequestVerificationToken']").val();
+
+        var email = $("#UserLoginModal input[name = 'Email']").val();
+        var password = $("#UserLoginModal input[name = 'Password']").val();
+        var rememberMe = $("#UserLoginModal input[name = 'RememberMe']").prop('checked');
+    }
+
+});
