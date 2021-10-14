162e083f0ce7d97a69b5ee675f7784b3fbd56ed9
Akram-Boktor <35446384+AkramBoktor@users.noreply.github.com>
2021-10-14 15:27:13 +0200

    Add files via upload

--- wwwroot/js/UserLogin.js
+++ wwwroot/js/UserLogin.js
@@ -1,14 +1,59 @@
-﻿$(function () {
-
-    var userLoginButton = $("#UserLoginModel button[name='login ']").click(onUserLoginClick);
+﻿
+$(function () {
+    var userLoginButton = $("#UserLoginModel button[name='login']").click(onUserLoginClick);
 
     function onUserLoginClick() {
+ 
         var userUrl = 'UserAuth/Login';
-        var antiForgeryToken = $("#UserLoginModal input[name='__RequestVerificationToken']").val();
+        var antiForgeryToken = $("#UserLoginModel input[name='__RequestVerificationToken']").val();
 
-        var email = $("#UserLoginModal input[name = 'Email']").val();
-        var password = $("#UserLoginModal input[name = 'Password']").val();
-        var rememberMe = $("#UserLoginModal input[name = 'RememberMe']").prop('checked');
+        var email = $("#UserLoginModel input[name = 'Email']").val();
+        var password = $("#UserLoginModel input[name = 'Password']").val();
+        var rememberMe = $("#UserLoginModel input[name = 'RememberMe']").prop('checked');
+      
+        var userInput = {
+            __RequestVerificationToken: antiForgeryToken,
+            Email: email,
+            Password: password,
+            RememberMe: rememberMe
+        };
+
+        $.ajax({
+            type: "POST",
+            url: userUrl,
+            data: userInput,
+            success: function (data) {
+                var parsed = $.parseHTML(data);
+                var hasErrors = $(parsed).find("input[name='LoginInValid']").val() == "true";
+
+                if (hasErrors == true) {
+                    $("#UserLoginModel").html(data);
+
+                    userLoginButton = $("#UserLoginModel button[name='login']").click(onUserLoginClick);
+
+                    var form = $("#UserLoginForm");
+
+                    $(form).removeData("validator");
+                    $(form).removeData("unobtrusiveValidation");
+                    $.validator.unobtrusive.parse(form);
+
+                }
+                else {
+                    location.href = 'Home/Index';
+
+                }
+            },
+      
+            error: function (xhr, ajaxOptions, thrownError) {
+                debugger;
+                var errorText = "Status: " + xhr.status + " - " + xhr.statusText;
+
+               PresentClosableBootstrapAlert("#alert_placeholder_login", "danger", "Error!", errorText);
+
+                console.error(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+            }
+
+        });
     }
 
 });
---
+++ wwwroot/js/UserRegistration.js
@@ -0,0 +1,115 @@
+﻿$(function () {
+
+
+    var registerUserButton = $("#UserRegisterationModel button[name = 'register']").click(onUserRegisterClick);
+
+    $("#UserRegisterationModel input[name = 'AcceptUserAgreement']").click(onAcceptUserArgeementClick);
+    $("#UserRegisterationModel button[name = 'register']").prop("disabled", true);
+
+    function onAcceptUserArgeementClick() {
+        if ($(this).is(":checked")) {
+            $("#UserRegisterationModel button[name = 'register']").prop("disabled", false);
+
+        } else {
+            $("#UserRegisterationModel button[name = 'register']").prop("disabled", true);
+
+        }
+    }
+
+    //check user name exists or not in database
+    $("#UserRegisterationModel input[name = 'Email']").blur(function () {
+
+        var email = $("#UserRegisterationModel input[name='Email']").val();
+        var url = "UserAuth/UserNameCheck?userName=" + email;
+
+        $.ajax({
+            type: "GET",
+            url: url,
+            success: function (data) {
+                if (data == true) {
+
+                    PresentClosableBootstrapAlert("#alert_placeholder_register", "warning", "Invalid Email", "This email address has already been registered");
+
+                }
+                else {
+                    CloseAlert("#alert_placeholder_register");
+                }
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                var errorText = "Status: " + xhr.status + " - " + xhr.statusText;
+
+                PresentClosableBootstrapAlert("#alert_placeholder_register", "danger", "Error!", errorText);
+
+                console.error(thrownError + '\r\n' + xhr.statusText + '\r\n' + xhr.responseText);
+
+            }
+        });
+
+    });
+
+    function onUserRegisterClick() {
+        var url = "UserAuth/RegisterUser";
+
+        var antiForgeryToken = $("#UserRegisterationModel input[name='__RequestVerificationToken']").val();
+        var email = $("#UserRegisterationModel input[name='Email']").val();
+        var password = $("#UserRegisterationModel input[name='Password']").val();
+        var confirmPassword = $("#UserRegisterationModel input[name='ConfirmPassword']").val();
+        var firstName = $("#UserRegisterationModel input[name='FirstName']").val();
+        var lastName = $("#UserRegisterationModel input[name='LastName']").val();
+        var address1 = $("#UserRegisterationModel input[name='Address1']").val();
+        var address2 = $("#UserRegisterationModel input[name='Address2']").val();
+        var postCode = $("#UserRegisterationModel input[name='PostCode']").val();
+        var phoneNumber = $("#UserRegisterationModel input[name='PhoneNumber']").val();
+
+        var user = {
+            __RequestVerificationToken: antiForgeryToken,
+            Email: email,
+            Password: password,
+            ConfirmPassword: confirmPassword,
+            FirstName: firstName,
+            LastName: lastName,
+            Address1: address1,
+            Address2: address2,
+            PostCode: postCode,
+            PhoneNumber: phoneNumber,
+            AcceptUserAgreement: true
+        };
+
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: user,
+            success: function (data) {
+
+                var parsed = $.parseHTML(data);
+
+                var hasErrors = $(parsed).find("input[name='RegistrationInValid']").val() == 'true';
+
+                if (hasErrors) {
+
+                    $("#UserRegisterationModel").html(data);
+                    var registerUserButton = $("#UserRegisterationModel button[name = 'register']").click(onUserRegisterClick);
+                    $("#UserRegisterationModel input[name = 'AcceptUserAgreement']").click(onAcceptUserAgreementClick);
+
+                    $("#UserRegistrationForm").removeData("validator");
+                    $("#UserRegistrationForm").removeData("unobtrusiveValidation");
+                    $.validator.unobtrusive.parse("#UserRegistrationForm");
+                }
+                else {
+                    location.href = '/Home/Index';
+                }
+
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                var errorText = "Status: " + xhr.status + " - " + xhr.statusText;
+
+                PresentClosableBootstrapAlert("#alert_placeholder_register", "danger", "Error!", errorText);
+
+                console.error(thrownError + '\r\n' + xhr.statusText + '\r\n' + xhr.responseText);
+            }
+
+        });
+
+    }
+
+});
